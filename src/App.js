c7a5deefd5213fc43b6e704d1cae5c73694eed42
Md Firoj Kabir <firoj.kabir65@gmail.com>
2020-07-29 15:47:26 +0200

    Create MemeGenerator component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import Header from './Header'
 
-import './App.css';
+import './App.css'
 
 class App extends Component {
 	
 	render() {
 		return (
 			<div>
-				<h1>Hello</h1>
+				<Header />
 			</div>
 		)
 	}
